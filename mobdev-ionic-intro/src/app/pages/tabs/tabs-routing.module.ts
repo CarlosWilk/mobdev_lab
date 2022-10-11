5c28584a39ff226cfc22d3147940a7b7457d27c4
Wilker Paulino <carloswilkersp@hotmail.com>
2022-10-11 21:17:59 +0000

    configured routing

--- mobdev-ionic-intro/src/app/pages/tabs/tabs-routing.module.ts
+++ mobdev-ionic-intro/src/app/pages/tabs/tabs-routing.module.ts
@@ -27,6 +27,10 @@ const routes: Routes = [
           {
             path: '',
             loadChildren: () => import('../people/people.module').then( m => m.PeoplePageModule)
+          },
+          {
+            path: ':id',
+            loadChildren: () => import('../person-details-page/person-details-page.module').then( m => m.PersonDetailsPagePageModule)
           }
         ]
       },
