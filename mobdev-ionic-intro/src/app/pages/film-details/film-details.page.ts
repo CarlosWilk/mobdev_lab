dbab3f42d17d6d79f6456c2c3c61c6d559b7da87
Wilker Paulino <carloswilkersp@hotmail.com>
2022-11-11 16:50:39 +0000

    changes applied

--- mobdev-ionic-intro/src/app/pages/film-details/film-details.page.ts
+++ mobdev-ionic-intro/src/app/pages/film-details/film-details.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from '../../services/api.service';
+import { FavouriteService } from './../../services/favourite.service';
 
 @Component({
   selector: 'app-film-details',
@@ -10,14 +11,34 @@ import { ApiService } from '../../services/api.service';
 export class FilmDetailsPage implements OnInit {
 
   film: any;
+  isFavorite = false;
+  filmId = null;
 
-  constructor(private activatedRoute: ActivatedRoute, private api: ApiService) { }
+  constructor(private activatedRoute: ActivatedRoute, private api: ApiService,  private favouriteService: FavouriteService) { }
 
   ngOnInit() {
     let id = this.activatedRoute.snapshot.paramMap.get('id');
     this.api.getFilm(id).subscribe(res => {
       this.film = res;
     });
+
+  this.favouriteService.isFavourite(this.filmId).then(isFav => {
+    this.isFavorite = isFav;
+  });
+
   }
+  
+favoriteFilm() {
+  this.favouriteService.favouriteFilm(this.filmId).then(() => {
+    this.isFavorite = true;
+  });
+}
+
+unfavoriteFilm() {
+  this.favouriteService.unfavouriteFilm(this.filmId).then(() => {
+    this.isFavorite = false;
+  });
+}
 
 }
+
